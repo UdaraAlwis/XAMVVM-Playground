17531a09c6b00eae8895fa71fba6a4782af91b0c
Udara Alwis <udara.robotics@gmail.com>
2018-10-23 21:38:38 +0800

    some prettiness

--- XFWithUITest/XFWithUITest/XFWithUITest/ViewModels/NewIdeaPageViewModel.cs
+++ XFWithUITest/XFWithUITest/XFWithUITest/ViewModels/NewIdeaPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace XFWithUITest.ViewModels
@@ -9,9 +10,25 @@ namespace XFWithUITest.ViewModels
     {
         private readonly INavigationService _navigationService;
 
+        public DelegateCommand SaveIdeaCommand { get; set; }
+
         public NewIdeaPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             this._navigationService = navigationService;
+
+            SaveIdeaCommand = new DelegateCommand(SaveIdea);
+        }
+
+        private void SaveIdea()
+        {
+            // Timer ended
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            // Timer started
         }
     }
 }
