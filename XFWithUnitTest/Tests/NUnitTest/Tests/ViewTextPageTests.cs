8b06cd625545547c46ef944c5d8228685b3893d2
Udara Alwis <udara.robotics@gmail.com>
2020-07-08 13:51:35 +0800

    rename screenshots

--- XFWithUnitTest/Tests/NUnitTest/Tests/ViewTextPageTests.cs
+++ XFWithUnitTest/Tests/NUnitTest/Tests/ViewTextPageTests.cs
@@ -2,8 +2,10 @@
 using NUnit.Framework;
 using Prism.Ioc;
 using Shouldly;
+using Xamarin.Forms;
 using XFWithUnitTest.Models;
 using XFWithUnitTest.ViewModels;
+using XFWithUnitTest.Views;
 
 namespace NUnitTest.Tests
 {
@@ -27,12 +29,7 @@ namespace NUnitTest.Tests
             App.Container.Resolve<HomePageViewModel>().NewTextCommand.Execute();
 
             // Create a new Text item
-            App.Container.Resolve<NewTextPageViewModel>().TextItem = new TextItem()
-            {
-                TextTitle = "Juis yuwe sjkl Tywe oiq aklsjd asqw al.",
-                Text = "Binf yuw tyasas pwerq asyu tui nuiwe aske yrwn kashdihas asju ywte.",
-            };
-            var newTextItem = App.Container.Resolve<NewTextPageViewModel>().TextItem;
+            var newTextItem = App.Container.Resolve<NewTextPageViewModel>().TextItem = CreateSampleTextItem();
 
             // I click on "SaveText" Button
             App.Container.Resolve<NewTextPageViewModel>().SaveTextCommand.Execute();
@@ -56,6 +53,71 @@ namespace NUnitTest.Tests
             viewingTextItem.Text.ShouldBe(newTextItem.Text);
             viewingTextItem.TextDateTime.ShouldBe(newTextItem.TextDateTime);
             viewingTextItem.LocationLatLong.ShouldBe(newTextItem.LocationLatLong);
+        }
+
+        /// <summary>
+        /// Navigating to the View Text Page from the Home
+        /// Page by selecting a Text item from the Text List
+        /// and Going back to the Home Page
+        /// </summary>
+        [Test]
+        public void ViewTextDetailsAndGoBackPageTest()
+        {
+            // Is the app running
+            App.ShouldNotBeNull();
+
+            // Am I in the Home page
+            GetCurrentPage().BindingContext.GetType().Name.ShouldBe(nameof(HomePageViewModel));
+
+            // Navigating to New Text page
+            App.Container.Resolve<HomePageViewModel>().NewTextCommand.Execute();
+
+            // Create a new Text item
+            var newTextItem = App.Container.Resolve<NewTextPageViewModel>().TextItem = CreateSampleTextItem();
+
+            // I click on "SaveText" Button
+            App.Container.Resolve<NewTextPageViewModel>().SaveTextCommand.Execute();
+
+            // Am I in the Home page
+            GetCurrentPage().BindingContext.GetType().Name.ShouldBe(nameof(HomePageViewModel));
+
+            // Check if we see new Text Item in the List
+            App.Container.Resolve<HomePageViewModel>().TextList.Count.ShouldBe(1);
+
+            // Select a Text item from the Text List
+            App.Container.Resolve<HomePageViewModel>().SelectedTextItem =
+                App.Container.Resolve<HomePageViewModel>().TextList.First();
+
+            // Am I in the View Text page
+            GetCurrentPage().BindingContext.GetType().Name.ShouldBe(nameof(ViewTextPageViewModel));
+
+            // Check the viewing Text item details
+            var viewingTextItem = App.Container.Resolve<ViewTextPageViewModel>().TextItem;
+            viewingTextItem.TextTitle.ShouldBe(newTextItem.TextTitle);
+            viewingTextItem.Text.ShouldBe(newTextItem.Text);
+            viewingTextItem.TextDateTime.ShouldBe(newTextItem.TextDateTime);
+            viewingTextItem.LocationLatLong.ShouldBe(newTextItem.LocationLatLong);
+
+            var textTitleLabel = ((Label)((Grid)((ViewTextPage)GetCurrentPage()).Content).Children.First());
+            textTitleLabel.Text.ShouldBe(newTextItem.TextTitle);
+
+            // Click "Done" button and navigate backwards
+            App.Container.Resolve<ViewTextPageViewModel>().DoneCommand.Execute();
+
+            // Am I in the Home page
+            App.Container.Resolve<HomePageViewModel>().TextList.Count.ShouldBe(1);
+        }
+
+        public TextItem CreateSampleTextItem()
+        {
+            // Create a new Text item
+            var textItem = new TextItem()
+            {
+                TextTitle = "Juis yuwe sjkl Tywe oiq aklsjd asqw al.",
+                Text = "Binf yuw tyasas pwerq asyu tui nuiwe aske yrwn kashdihas asju ywte.",
+            };
+
+            return textItem;
         }
     }
 }
